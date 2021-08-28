3e8236551446ec34d4cfb3a0503f37c41c44847a
Armen_Gasparyan <armen_gasparyan@epam.com>
2021-08-28 15:55:09 +0200

    workaround

--- 7-gce/config.py
+++ 7-gce/config.py
@@ -32,12 +32,12 @@ DATA_BACKEND = 'cloudsql'
 
 # Google Cloud Project ID. This can be found on the 'Overview' page at
 # https://console.developers.google.com
-PROJECT_ID = 'your-project-id'
+PROJECT_ID = 'osdu-deploy-gasparyan'
 
 # CloudSQL & SQLAlchemy configuration
 # Replace the following values the respective values of your Cloud SQL
 # instance.
-CLOUDSQL_USER = 'root'
+CLOUDSQL_USER = 'bookshelf-user'
 CLOUDSQL_PASSWORD = '12345678'
 CLOUDSQL_DATABASE = 'db'
 # Set this value to the Cloud SQL connection name, e.g.
@@ -59,3 +59,52 @@ LOCAL_SQLALCHEMY_DATABASE_URI = (
         user=CLOUDSQL_USER, password=CLOUDSQL_PASSWORD,
         database=CLOUDSQL_DATABASE)
 
+# When running on App Engine a unix socket is used to connect to the cloudsql
+# instance.
+LIVE_SQLALCHEMY_DATABASE_URI = (
+    'mysql+pymysql://{user}:{password}@localhost/{database}'
+    '?unix_socket=/cloudsql/{connection_name}').format(
+        user=CLOUDSQL_USER, password=CLOUDSQL_PASSWORD,
+        database=CLOUDSQL_DATABASE, connection_name=CLOUDSQL_CONNECTION_NAME)
+
+if os.environ.get('GAE_INSTANCE'):
+    SQLALCHEMY_DATABASE_URI = LIVE_SQLALCHEMY_DATABASE_URI
+else:
+    SQLALCHEMY_DATABASE_URI = LOCAL_SQLALCHEMY_DATABASE_URI
+
+# Mongo configuration
+# If using mongolab, the connection URI is available from the mongolab control
+# panel. If self-hosting on compute engine, replace the values below.
+MONGO_URI = 'mongodb://user:password@host:27017/database'
+
+# Google Cloud Storage and upload settings.
+# Typically, you'll name your bucket the same as your project. To create a
+# bucket:
+#
+#   $ gsutil mb gs://<your-bucket-name>
+#
+# You also need to make sure that the default ACL is set to public-read,
+# otherwise users will not be able to see their upload images:
+#
+#   $ gsutil defacl set public-read gs://<your-bucket-name>
+#
+# You can adjust the max content length and allow extensions settings to allow
+# larger or more varied file types if desired.
+CLOUD_STORAGE_BUCKET = 'your-bucket-name'
+MAX_CONTENT_LENGTH = 8 * 1024 * 1024
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+# OAuth2 configuration.
+# This can be generated from the Google Developers Console at
+# https://console.developers.google.com/project/_/apiui/credential.
+# Note that you will need to add all URLs that your application uses as
+# authorized redirect URIs. For example, typically you would add the following:
+#
+#  * http://localhost:8080/oauth2callback
+#  * https://<your-app-id>.appspot.com/oauth2callback.
+#
+# If you receive a invalid redirect URI error review you settings to ensure
+# that the current URI is allowed.
+GOOGLE_OAUTH2_CLIENT_ID = \
+    'your-client-id'
+GOOGLE_OAUTH2_CLIENT_SECRET = 'your-client-secret'
